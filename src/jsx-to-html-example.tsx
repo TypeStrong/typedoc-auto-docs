5650707f1aa7a90f869847ff28cdf8d868f11b14
Andrew Bradley <cspotcode@gmail.com>
2021-07-14 22:46:28 -0400

    Correctly classify "external" types in rendered docs. Also add example of rendering JSX to HTML, and defensively prevent typedoc from accidentally reading any stray typedoc config files

---
+++ src/jsx-to-html-example.tsx
@@ -0,0 +1,10 @@
+// I always forget how to do this, so this stub is for my own reference.
+// In work.ts I generate some index.html files, and it would be nice to
+// do that with JSX.
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+
+ReactDOMServer.renderToStaticMarkup(
+    <html>
+    </html>
+);
