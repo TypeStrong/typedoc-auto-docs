5573edce98b3490007f2d683380a9ad5683389a2
Andrew Bradley <cspotcode@gmail.com>
2021-07-18 03:28:13 -0400

    fix formatting

--- src/unpkg-plugin/UnpkgPlugin.js
+++ src/unpkg-plugin/UnpkgPlugin.js
@@ -1,13 +1,13 @@
 require('ts-node').register({
-    projectSearchDir: __dirname
+  projectSearchDir: __dirname,
 });
 const { UnpkgPlugin } = require('./UnpkgPluginImpl');
 
 module.exports = function (PluginHost) {
-    var app = PluginHost.owner;
+  var app = PluginHost.owner;
 
-    // app.options.addDeclaration({name: 'sourcefile-url-map'});
-    // app.options.addDeclaration({name: 'sourcefile-url-prefix'});
+  // app.options.addDeclaration({name: 'sourcefile-url-map'});
+  // app.options.addDeclaration({name: 'sourcefile-url-prefix'});
 
-    app.converter.addComponent('unpkg-plugin', UnpkgPlugin);
-}
+  app.converter.addComponent('unpkg-plugin', UnpkgPlugin);
+};
